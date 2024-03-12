614cd340f5ea0a60437619ff351ec6f3c76df0fd
Antochevis <20241GR.ADS0016@gravatai.ifsul.edu.br>
2024-03-12 20:39:33 -0300

    atualização programa 2

---
+++ algoritimos_logicaProgramacao/primeiro_programa_C.cpp
@@ -0,0 +1,16 @@
+/* isto é um comentário
+teste */
+
+// teste
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Um Primeiro Programa */
+int main ()
+{
+	printf("Hello world\n");
+	system("pause");
+	return(0);
+}
+
---
+++ algoritimos_logicaProgramacao/segundo_programa_C.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int x;
+	scanf("%d",&x);
+	x=x*2;
+	printf("%d\n",x);
+	system("pause");
+	return(0);
+} 
